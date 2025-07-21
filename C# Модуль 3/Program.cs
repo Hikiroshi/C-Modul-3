55f709f4d1526cdbbd996691a3aaa72c48b95983
Roman <brawlst2007@gmail.com>
2025-07-21 20:24:12 +0300

    перечисление дней недели

--- C# Модуль 3/Program.cs
+++ C# Модуль 3/Program.cs
@@ -1,19 +1,16 @@
 ﻿using System;
 
 class MainClass
+
 {
-    public static void Main(string[] args)
+    enum DaysOfWeek : byte
     {
-
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 37.5;
-
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
-
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
