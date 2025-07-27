75935160044bebbeaf7ad04e24067c329fe4f9aa
Roman <brawlst2007@gmail.com>
2025-07-28 00:06:43 +0300

    task 4.1.4

--- C# Модуль 3/Program.cs
+++ C# Модуль 3/Program.cs
@@ -4,16 +4,9 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        Console.Write("Введите имя: ");
-        var name = Console.ReadLine();
-
-        Console.WriteLine("введите возраст: ");
-        var age = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Ваше имя {0}, возвраст {1} лет", name, age);
-
-        Console.WriteLine("введите дату рождения: ");
-        var birthdate = Console.ReadLine();
-        Console.WriteLine("Ваше день рождения {0}", birthdate);
+        string a;
+        string b;
+        var c = a != b;
+        Console.WriteLine(c);
     }
 }
